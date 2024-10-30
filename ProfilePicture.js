0ecf66ea85ebbc1f0e1f52e7bf20babbba443c08
Aarti Pawar <aartipawar965@gmail.com>
2024-10-30 15:28:02 +0530

    Assignment no 3

---
+++ ProfilePicture.js
@@ -0,0 +1,12 @@
+// src/ProfilePicture.js
+import React from 'react';
+
+const ProfilePicture = ({ imageUrl }) => {
+    return (
+        <div className="profile-picture">
+            
+        </div>
+    );
+};
+
+export default ProfilePicture;
